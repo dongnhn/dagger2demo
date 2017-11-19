123bfc830084c942a5696d8927d441f105b9b669
Dong <nguyen.dong@2359media.com>
2017-11-19 22:35:57 +0700

    init an instance of AppComponent and store in application to use later

--- app/src/main/java/dong/media2359/dagger2demo/DemoApplication.java
+++ app/src/main/java/dong/media2359/dagger2demo/DemoApplication.java
@@ -2,13 +2,27 @@ package dong.media2359.dagger2demo;
 
 import android.app.Application;
 
+import dong.media2359.dagger2demo.di.AppComponent;
+import dong.media2359.dagger2demo.di.AppModule;
+import dong.media2359.dagger2demo.di.DaggerAppComponent;
+
 /**
  * Created by Dong (nguyen.dong@2359media.com) on 6/9/17.
  */
 
 public class DemoApplication extends Application {
+
+    private AppComponent appComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        appComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent getAppComponent() {
+        return appComponent;
     }
 }
