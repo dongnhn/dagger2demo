b24903f472babd98fab7011a328fca03a36b1b66
Dong <nguyen.dong@2359media.com>
2017-11-19 22:53:44 +0700

    declare subcomponent in parent component (AppComponent)

--- app/src/main/java/dong/media2359/dagger2demo/di/AppComponent.java
+++ app/src/main/java/dong/media2359/dagger2demo/di/AppComponent.java
@@ -1,7 +1,9 @@
 package dong.media2359.dagger2demo.di;
 
 import dagger.Component;
+import dong.media2359.dagger2demo.home.HomeComponent;
 import dong.media2359.dagger2demo.imageloader.ImageLoader;
+import dong.media2359.dagger2demo.home.HomeModule;
 
 /**
  * Created by Dong (nguyen.dong@2359media.com) on 11/19/17.
@@ -10,4 +12,6 @@ import dong.media2359.dagger2demo.imageloader.ImageLoader;
 @Component(modules = {AppModule.class})
 public interface AppComponent {
     ImageLoader getImageLoader();
+    // Dagger doesn't know how to create HomeModule, so we must provide it in constructor
+    HomeComponent homeComponent(HomeModule homeModule);
 }
